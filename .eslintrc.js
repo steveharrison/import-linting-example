40ce638090d78ee4930eb69a5609e72d5eab6f16
Steve Harrison <steveharrison.dev@icloud.com>
2021-08-16 15:56:59 +1000

    Example project with import linting not working.
    I expect all of the imports to fail as they reference an invalid export property of the module, but no import ESLint errors are thrown.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+  settings: {
+    'import/resolver': 'parcel',
+    'import/no-unresolved': [2, {commonjs: true, amd: true}],
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { doesNotExist } from 'react';
+import { doesNotExist2 } from 'redux';
+import { doesNotExist3 } from '../dist/external';
